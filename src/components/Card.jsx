f0286883f7bd4dc81f31ec5a5b8608e0a27f7b20
Diksha 2307 <dikshasinghal239@gmail.com>
2021-05-21 20:29:35 +0530

    1. Import the contacts.js file to create card components 2.Use props to render the default Beyonce contact card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 function Card(props){
     return (
-        <div>
-      <h1 className="heading">My Contacts</h1>
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
@@ -17,7 +15,7 @@ function Card(props){
           <p className="info">{props.email}</p>
         </div>
       </div>
-    </div>
+
     )
 }
 
